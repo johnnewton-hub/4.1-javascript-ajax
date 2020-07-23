ce8f2ff4c1230341fbcba2314efb3a773f86a013
Warren Uhrich <hello@warren.codes>
2020-07-23 12:44:12 -0600

    Add fetch request for all astronauts in space; console log result data.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,13 @@
  * AJAX Examples
  */
 
-// Retrieving an external resource (text file.)
-fetch( "https://www.w3schools.com/xml/ajax_info.txt" ) // Sends the request...
-  // We can use .then() to parse the response.
-  .then( response => {
-    console.log( response );
-  } )
-
 // Retrieving an external resource (live JSON data.)
 fetch( "http://api.open-notify.org/astros.json" ) // Sends the request...
   // We can use .then() to parse the response.
   .then( response => {
-    console.log( response );
+    return response.json()
+  } )
+  // Data has been formatted, let's have a look inside...
+  .then( data => {
+    console.log( data )
   } )
