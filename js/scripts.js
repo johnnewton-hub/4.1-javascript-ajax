8d110d3e39cf6c45e116559bee9bf69a6fd4c028
Warren Uhrich <hello@warren.codes>
2020-07-23 13:09:43 -0600

    Add output for the astronauts as a <ul><li> list.

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,19 @@ fetch( "http://api.open-notify.org/astros.json" ) // Sends the request...
   .then( data => {
     // Output data to console for testing.
     console.log( data );
-    // 
-  } )
+    // Prepare for HTML list.
+    const peopleUL = document.createElement( "UL" );
+    // Let's grab those people!
+    const people = data.people; // This is an array of people...
+    // Let's loop through them!
+    for ( const person of people )
+    { // Prepare an LI for this person.
+      const personLI = document.createElement( "LI" );
+      // Add some text content (using template literal to inject our values.)
+      personLI.textContent = `${person.name} is currently aboard ${person.craft}.`;
+      // Add this <li> to our <ul>.
+      peopleUL.appendChild( personLI );
+    }
+    // Add our <ul> to the <body> so we can see it in the browser!
+    document.body.appendChild( peopleUL );
+  } );
